4f56cc6cb011459ee18d5fc53593bce97c410049
Benjamin Gray <bengray178@gmail.com>
2019-12-12 17:42:10 +1000

    support travis windows

--- script/build.js
+++ script/build.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 function build() {
   if (process.platform === "win32") {
-    console.error("Can't build for windows yet");
+    cp.execSync('"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\Common7\\Tools\\VsDevCmd.bat" && make_win.bat', {cwd: path.resolve(__dirname, "..", "deps", "oniguruma"), stdio: "inherit"});
   } else {
     cp.execSync("autoreconf -vfi", {cwd: path.resolve(__dirname, "..", "third_party", "oniguruma"), stdio: "inherit"});
     cp.execSync("./configure", {cwd: path.resolve(__dirname, "..", "third_party", "oniguruma"), stdio: "inherit"});
