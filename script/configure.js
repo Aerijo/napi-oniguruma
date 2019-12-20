14dd29659da3d00907cc8c2e92a3a5544283b550
Benjamin Gray <bengray178@gmail.com>
2019-12-20 23:13:56 +1000

    simplify configure command

--- script/configure.js
+++ script/configure.js
@@ -5,8 +5,7 @@ function configure() {
   if (process.platform === "win32") {
     cp.execSync('"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\BuildTools\\Common7\\Tools\\VsDevCmd.bat" && make_win.bat', {cwd: path.resolve(__dirname, "..", "third_party", "oniguruma"), stdio: "inherit"});
   } else {
-    cp.execSync("autoreconf -vfi", {cwd: path.resolve(__dirname, "..", "third_party", "oniguruma"), stdio: "inherit"});
-    cp.execSync("./configure", {cwd: path.resolve(__dirname, "..", "third_party", "oniguruma"), stdio: "inherit"});
+    cp.execSync("autoreconf -vfi && ./configure", {cwd: path.resolve(__dirname, "..", "third_party", "oniguruma"), stdio: "inherit"});
   }
 }
 
